104528705dfa80c62d17808fe1c6aabe5aa3b503
Ademide-e <159146744+Ademide-e@users.noreply.github.com>
2024-03-14 09:16:03 +0100

    Add files via upload

---
+++ landing/landing.js
@@ -0,0 +1,121 @@
+function health(){
+    document.getElementById("bhealth").style.display = "block"
+    document.getElementById("bauto").style.display = "none"
+    document.getElementById("bgadget").style.display = "none"
+    document.getElementById("btravel").style.display = "none"
+    document.getElementById("hb").style.backgroundColor = "#ECFDF3"
+    document.getElementById("ab").style.backgroundColor = "none"
+    document.getElementById("gb").style.backgroundColor = "none"
+    document.getElementById("tb").style.backgroundColor = "none"
+}
+function auto(){
+    document.getElementById("bhealth").style.display = "none"
+    document.getElementById("bauto").style.display = "block"
+    document.getElementById("bgadget").style.display = "none"
+    document.getElementById("btravel").style.display = "none"
+    document.getElementById("ab").style.backgroundColor = "#ECFDF3"
+    document.getElementById("hb").style.backgroundColor = "none"
+    document.getElementById("gb").style.backgroundColor = "none"
+    document.getElementById("tb").style.backgroundColor = "none"
+}
+function gadget(){
+    document.getElementById("bhealth").style.display = "none"
+    document.getElementById("bauto").style.display = "none"
+    document.getElementById("bgadget").style.display = "block"
+    document.getElementById("btravel").style.display = "none"
+    document.getElementById("gb").style.backgroundColor = "#ECFDF3"
+    document.getElementById("hb").style.backgroundColor = "none"
+    document.getElementById("ab").style.backgroundColor = "none"
+    document.getElementById("tb").style.backgroundColor = "none"
+}
+function travel(){
+    document.getElementById("bhealth").style.display = "none"
+    document.getElementById("bauto").style.display = "none"
+    document.getElementById("bgadget").style.display = "none"
+    document.getElementById("btravel").style.display = "block"
+    document.getElementById("tb").style.backgroundColor = "#ECFDF3"
+    document.getElementById("hb").style.backgroundColor = "none"
+    document.getElementById("ab").style.backgroundColor = "none"
+    document.getElementById("gb").style.backgroundColor = "none"
+}
+function que1(){
+    document.getElementById("question1").style.display = "none"
+    document.getElementById("answer1").style.display = "flex"
+    document.getElementById("question2").style.display = "flex"
+    document.getElementById("answer2").style.display = "none"
+    document.getElementById("question3").style.display = "flex"
+    document.getElementById("answer3").style.display = "none"
+    document.getElementById("question4").style.display = "flex"
+    document.getElementById("answer4").style.display = "none"
+}
+function que2(){
+    document.getElementById("question1").style.display = "flex"
+    document.getElementById("answer1").style.display = "none"
+    document.getElementById("question2").style.display = "none"
+    document.getElementById("answer2").style.display = "flex"
+    document.getElementById("question3").style.display = "flex"
+    document.getElementById("answer3").style.display = "none"
+    document.getElementById("question4").style.display = "flex"
+    document.getElementById("answer4").style.display = "none"
+}
+function que3(){
+    document.getElementById("question1").style.display = "flex"
+    document.getElementById("answer1").style.display = "none"
+    document.getElementById("question2").style.display = "flex"
+    document.getElementById("answer2").style.display = "none"
+    document.getElementById("question3").style.display = "none"
+    document.getElementById("answer3").style.display = "flex"
+    document.getElementById("question4").style.display = "flex"
+    document.getElementById("answer4").style.display = "none"
+}
+function que4(){
+    document.getElementById("question1").style.display = "flex"
+    document.getElementById("answer1").style.display = "none"
+    document.getElementById("question2").style.display = "flex"
+    document.getElementById("answer2").style.display = "none"
+    document.getElementById("question3").style.display = "flex"
+    document.getElementById("answer3").style.display = "none"
+    document.getElementById("question4").style.display = "none"
+    document.getElementById("answer4").style.display = "flex"
+}
+function pitem1(){
+    document.getElementById("p1").style.display = "none"
+    document.getElementById("p2").style.display = "flex"
+    document.getElementById("p3").style.display = "flex"
+    document.getElementById("p4").style.display = "none"
+    document.getElementById("p5").style.display = "flex"
+    document.getElementById("p6").style.display = "none"
+    document.getElementById("icon1").src = "Frame 93104.png"
+    document.getElementById("icon2").src = "Frame 93108.png"
+    document.getElementById("icon3").src = "Frame 93109.png"
+    document.getElementById("iconImage").src = "Frame 628204.png"
+}
+function pitem2(){
+    document.getElementById("p1").style.display = "flex"
+    document.getElementById("p2").style.display = "none"
+    document.getElementById("p3").style.display = "none"
+    document.getElementById("p4").style.display = "flex"
+    document.getElementById("p5").style.display = "flex"
+    document.getElementById("p6").style.display = "none"
+    document.getElementById("icon2").src = "icon2.png"
+    document.getElementById("icon1").src = "icon1.png"
+    document.getElementById("icon3").src = "Frame 93109.png"
+    document.getElementById("iconImage").src = "iconimage2.png"
+}
+function pitem3(){
+    document.getElementById("p1").style.display = "flex"
+    document.getElementById("p2").style.display = "none"
+    document.getElementById("p3").style.display = "flex"
+    document.getElementById("p4").style.display = "none"
+    document.getElementById("p5").style.display = "none"
+    document.getElementById("p6").style.display = "flex"
+    document.getElementById("icon3").src = "icon3.png"
+    document.getElementById("icon1").src = "icon1.png"
+    document.getElementById("icon2").src = "Frame 93109.png"
+    document.getElementById("iconImage").src = "iconimage3.png"
+}
+function changeIcon1(){}
+function changeIcon2(){}
+function changeIcon3(){}
+
+const list = []
